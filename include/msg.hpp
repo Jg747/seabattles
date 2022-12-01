10533c8b72ea07b6dfdb04e3b9a2291ad25a9557
Jg_747 <Jg747official@gmail.com>
2022-12-01 22:38:32 +0100

    Added some parsing functions, still needing tests for creation

--- include/msg.hpp
+++ include/msg.hpp
@@ -82,125 +82,119 @@ struct match_stats_t {
 	struct stats_t info;
 };
 
-struct c_player_get_own_id {
+struct player_get_own_id {
 	std::string username;
 };
 
-struct c_player_ship_placement {
+struct player_ship_placement {
 	struct ship_t array[SHIPS_COUNT];
 };
 
-struct c_player_get_board {
+struct player_get_board {
 	int id;
 };
 
-struct c_player_attack {
+struct player_attack {
 	struct player_info player;
 	int x;
 	int y;
 };
 
-struct c_host_player_kick {
+struct host_player_kick {
 	struct player_info player;
 	std::string message;
 };
 
-typedef struct player_info c_ack_player_get_own_id;
+typedef struct player_info ack_player_get_own_id;
 
-struct c_ack_match_attack_status {
+struct ack_match_attack_status {
 	struct player_info player;
 	enum attack_status_e status;
 };
 
-struct c_ack_get_board {
+struct ack_get_board {
 	struct player_info client;
 	struct player_info player;
 	struct board_t board;
 };
 
-typedef struct match_stats_t c_ack_match_end;
+typedef struct match_stats_t ack_match_end;
 
-typedef struct player_info c_ack_invalid_ship_placement;
+typedef struct player_info ack_invalid_ship_placement;
 
-struct c_ack_match_attack_err {
+struct ack_match_attack_err {
 	struct player_info player;
 	enum attack_status_e status;
 };
 
-typedef struct player_info c_ack_match_not_host;
+typedef struct player_info ack_match_not_host;
 
-typedef struct player_info c_ack_match_not_dead;
+typedef struct player_info ack_match_not_dead;
 
-struct c_msg_player_list {
+struct msg_player_list {
 	std::vector<struct player_info> array;
 };
 
-struct c_match_player_removed {
+struct match_player_removed {
 	struct player_info player;
 	std::string reason;
-	struct c_msg_player_list list;
+	struct msg_player_list list;
 };
 
-struct c_match_all_placed {
+struct match_all_placed {
 	bool turn;
 };
 
-struct c_match_new_board {
+struct match_new_board {
 	struct player_info player;
 	struct player_info attacker;
 	struct board_t board;
 };
 
-typedef struct match_stats_t c_match_end;
+typedef struct match_stats_t match_end, match_win, match_lose;
 
-typedef struct match_stats_t c_match_win;
-
-typedef struct match_stats_t c_match_lose;
-
-struct c_match_got_kicked {
+struct match_got_kicked {
 	std::string reason;
 };
 
-struct msg_creation {
+struct msg {
+	enum msg_type_e msg_type;
+
 	union {
-		struct c_player_get_own_id player_get_own_id;
-		struct c_player_ship_placement player_ship_placement;
-		struct c_player_get_board player_get_board;
-		struct c_player_get_board player_get_board_lost;
-		struct c_player_attack player_attack;
+		struct player_get_own_id player_get_own_id;
+		struct player_ship_placement player_ship_placement;
+		struct player_get_board player_get_board;
+		struct player_get_board player_get_board_lost;
+		struct player_attack player_attack;
 		
-		struct c_host_player_kick host_player_kick;
+		struct host_player_kick host_player_kick;
 		
-		c_ack_player_get_own_id ack_player_get_own_id;
-		struct c_ack_match_attack_status ack_match_attack_status;
-		struct c_ack_get_board ack_get_board;
-		struct c_ack_get_board ack_get_board_lost;
-		c_ack_match_end ack_match_end;
-		c_ack_invalid_ship_placement ack_invalid_ship_placement;
-		struct c_ack_match_attack_err ack_match_attack_err;
-		c_ack_match_not_host ack_match_not_host;
-		c_ack_match_not_dead ack_match_not_dead;
+		ack_player_get_own_id ack_player_get_own_id;
+		struct ack_match_attack_status ack_match_attack_status;
+		struct ack_get_board ack_get_board;
+		struct ack_get_board ack_get_board_lost;
+		ack_match_end ack_match_end;
+		ack_invalid_ship_placement ack_invalid_ship_placement;
+		struct ack_match_attack_err ack_match_attack_err;
+		ack_match_not_host ack_match_not_host;
+		ack_match_not_dead ack_match_not_dead;
 	
-		struct c_msg_player_list msg_player_list;
-		struct c_match_player_removed match_player_removed;
-		struct c_match_all_placed match_all_placed;
-		struct c_match_new_board match_new_board;
-		c_match_win match_win;
-		c_match_lose match_lose;
-		c_match_end match_end;
-		struct c_match_got_kicked match_got_kicked;
+		struct msg_player_list msg_player_list;
+		struct match_player_removed match_player_removed;
+		struct match_all_placed match_all_placed;
+		struct match_new_board match_new_board;
+		match_win match_win;
+		match_lose match_lose;
+		match_end match_end;
+		struct match_got_kicked match_got_kicked;
 	} data;
 };
 
-struct msg_parsing {
-	union {
-		
-	} data;
-};
+typedef struct msg msg_creation, msg_parsing;
 
-std::string create_message(enum msg_type_e type, int ack_id);
-std::string create_message(enum msg_type_e type, struct msg_creation *msg, int ack_id);
-void parse_message(pugi::xml_document &doc, enum msg_type_e type, struct msg_parsing *msg);
-enum msg_type_e get_msg_type(std::string &str);
+std::string create_message(enum msg_type_e type);
+std::string create_message(enum msg_type_e type, msg_creation *msg);
+void parse_message(std::string &xml_message, msg_parsing *msg);
+enum msg_type_e get_msg_type(pugi::xml_document &doc);
 
 #endif
--- src/msg.cpp
+++ src/msg.cpp
@@ -17,7 +17,7 @@
 const char *MSG_TYPE_STR[] = {
 	// Generici
 	"GENERIC",
-	"NAK",
+	"nak",
 
 	// Connessione
 	"MSG_CONN_ACCEPTED",
@@ -71,6 +71,15 @@ const char *ATTACK_STATUS_STR[] = {
 	"DEAD_CANNOT_ATTACK",
 	"INVALID_ATTACK"
 };
+
+static int get_enum(std::string str, const char *arr[], size_t len) {
+	for (int i = 0; i < len; i++) {
+		if (!strcmp(str.c_str(), arr[i])) {
+			return i;
+		}
+	}
+	return -1;
+}
 
 template<typename T>
 static bool is_a_string(T value) {
@@ -215,12 +224,12 @@ static void create_conn_match_started(pugi::xml_document &doc) {
 	create_ack_node(data, MSG_CONN_MATCH_STARTED);
 }
 
-static void create_player_get_own_id(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_player_get_own_id(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "username", msg->data.player_get_own_id.username);
 }
 
-static void create_player_ship_placement(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_player_ship_placement(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 
 	for (int i = 0; i < SHIPS_COUNT; i++) {
@@ -233,43 +242,43 @@ static void create_player_ship_placement(pugi::xml_document &doc, struct msg_cre
 	}
 }
 
-static void create_player_get_board(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_player_get_board(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "id", msg->data.player_get_board.id);
 }
 
-static void create_player_get_board_lost(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_player_get_board_lost(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "id", msg->data.player_get_board_lost.id);
 }
 
-static void create_player_attack(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_player_attack(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "id", msg->data.player_attack.player.player_id);
 	add_node(data, "x", msg->data.player_attack.x);
 	add_node(data, "y", msg->data.player_attack.y);
 }
 
-static void create_host_player_kick(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_host_player_kick(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "id", msg->data.host_player_kick.player.player_id);
 	add_node(data, "msg", msg->data.host_player_kick.message);
 }
 
-static void create_ack_player_get_own_id(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_ack_player_get_own_id(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	create_ack_node(data, ACK_MSG_PLAYER_GET_OWN_ID);
 	add_node(data, "id", msg->data.ack_player_get_own_id.player_id);
 }
 
-static void create_ack_match_attack_status(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_ack_match_attack_status(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "id", msg->data.ack_match_attack_status.player.player_id);
 	create_ack_node(data, ACK_MSG_MATCH_ATTACK_STATUS);
 	add_node(data, "status", ATTACK_STATUS_STR[msg->data.ack_match_attack_status.status]);
 }
 
-static void create_ack_get_board(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_ack_get_board(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "id", msg->data.ack_get_board.client.player_id);
 	create_ack_node(data, ACK_MSG_GET_BOARD);
@@ -277,7 +286,7 @@ static void create_ack_get_board(pugi::xml_document &doc, struct msg_creation *m
 	append_board(data, &msg->data.ack_get_board.board);
 }
 
-static void create_ack_get_board_lost(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_ack_get_board_lost(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "id", msg->data.ack_get_board_lost.client.player_id);
 	create_ack_node(data, ACK_MSG_GET_BOARD);
@@ -285,86 +294,89 @@ static void create_ack_get_board_lost(pugi::xml_document &doc, struct msg_creati
 	append_board(data, &msg->data.ack_get_board_lost.board);
 }
 
-static void create_ack_match_end(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_ack_match_end(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "id", msg->data.ack_match_end.player.player_id);
 	create_ack_node(data, ACK_MSG_MATCH_END);
 	append_info(data, &msg->data.ack_match_end.info);
 }
 
-static void create_ack_invalid_ship_placement(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_ack_invalid_ship_placement(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "id", msg->data.ack_invalid_ship_placement.player_id);
 	create_ack_node(data, ACK_INVALID_SHIP_PLACEMENT);
 }
 
-static void create_ack_match_attack_err(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_ack_match_attack_err(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "id", msg->data.ack_match_attack_err.player.player_id);
 	create_ack_node(data, ACK_MSG_MATCH_ATTACK_ERR);
 	add_node(data, "status", (int)msg->data.ack_match_attack_err.status);
 }
 
-static void create_ack_match_not_host(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_ack_match_not_host(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "id", msg->data.ack_match_not_host.player_id);
 	create_ack_node(data, ACK_MSG_MATCH_NOT_HOST);
 }
 
-static void create_ack_match_not_dead(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_ack_match_not_dead(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "id", msg->data.ack_match_not_dead.player_id);
 	create_ack_node(data, ACK_MSG_MATCH_NOT_DEAD);
 }
 
-static void create_player_list(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_player_list(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	append_list(data, &msg->data.msg_player_list);
 }
 
-static void create_match_player_removed(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_match_player_removed(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "who", msg->data.match_player_removed.player.name);
 	add_node(data, "reason", msg->data.match_player_removed.reason);
 	append_list(data, &msg->data.match_player_removed.list);
 }
 
-static void create_match_all_placed(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_match_all_placed(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "turn", msg->data.match_all_placed.turn);
 }
 
-static void create_match_new_board(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_match_new_board(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "id", msg->data.match_new_board.player.player_id);
 	add_node(data, "attacker", msg->data.match_new_board.attacker.name);
 	append_board(data, &msg->data.match_new_board.board);
 }
 
-static void create_match_win(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_match_win(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "id", msg->data.match_win.player.player_id);
 	append_info(data, &msg->data.match_win.info);
 }
 
-static void create_match_lose(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_match_lose(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "id", msg->data.match_lose.player.player_id);
 	append_info(data, &msg->data.match_lose.info);
 }
 
-static void create_match_end(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_match_end(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "id", msg->data.match_end.player.player_id);
 	append_info(data, &msg->data.match_end.info);
 }
 
-static void create_match_got_kicked(pugi::xml_document &doc, struct msg_creation *msg) {
+static void create_match_got_kicked(pugi::xml_document &doc, msg_creation *msg) {
 	pugi::xml_node data = get_data_node(doc);
 	add_node(data, "reason", msg->data.match_got_kicked.reason);
 }
 
-std::string create_message(enum msg_type_e type, struct msg_creation *msg, int ack_id) {
+std::string create_message(enum msg_type_e type, msg_creation *msg) {
+	if (msg != NULL) {
+		msg->msg_type = type;
+	}
 	pugi::xml_document doc = init_msg(type);
 
 	switch (type) {
@@ -465,7 +477,7 @@ std::string create_message(enum msg_type_e type, struct msg_creation *msg, int a
 	return close_msg(doc);
 }
 
-std::string create_message(enum msg_type_e type, int ack_id) {
+std::string create_message(enum msg_type_e type) {
 	return create_message(type, NULL, ack_id);
 }
 
@@ -473,10 +485,198 @@ std::string create_message(enum msg_type_e type, int ack_id) {
 //                      MSG PARSING                     
 // ----------------------------------------------------
 
-void parse_message(pugi::xml_document &doc, enum msg_type_e type, struct msg_parsing *msg) {
-	// ...
+enum msg_type_e get_msg_type(pugi::xml_document &doc) {
+	pugi::xml_node type = doc.child("message").child("type");
+	std::string msg_type = type.value();
+	if (msg_type != "ack") {
+		return (enum msg_type_e)get_enum(msg_type, MSG_TYPE_STR, MSG_TYPE_STR_LEN);
+	} else {
+		pugi::xml_node ack_type = get_data_node(doc).child("acktype");
+		msg_type = ack_type.value();
+		return (enum msg_type_e)get_enum(msg_type, MSG_TYPE_STR, MSG_TYPE_STR_LEN);
+	}
 }
 
-enum msg_type_e get_msg_type(std::string str) {
-	// ...
+static void parse_player_get_own_id(pugi::xml_document &doc, msg_parsing *msg) {
+	msg->data.player_get_own_id.username = get_data_node(doc).child("username").value();
+}
+
+static void parse_player_ship_placement(pugi::xml_document &doc, msg_parsing *msg) {
+	pugi::xml_node data = get_data_node(doc);
+	size_t i = 0;
+	for (pugi::xml_node ship_node = data.first_child(); ship_node; ship_node = data.next_sibling(), i++) {
+		struct ship_t *ship = &msg->data.player_ship_placement.array[i];
+		ship->type = (enum ship_e)strtol(ship_node.child("type").value(), NULL, 10);
+		ship->x = (int)strtol(ship_node.child("x").value(), NULL, 10);
+		ship->y = (int)strtol(ship_node.child("y").value(), NULL, 10);
+		ship->rotation = (enum rotation_e)strtol(ship_node.child("orientation").value(), NULL, 10);
+	}
+}
+
+static void parse_player_get_board(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_player_get_board_lost(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_player_attack(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_host_player_kick(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_ack_player_get_own_id(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_ack_match_attack_status(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_ack_get_board(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_ack_get_board_lost(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_ack_match_end(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_ack_invalid_ship_placement(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_ack_match_attack_err(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_ack_match_not_host(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_ack_match_not_dead(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_player_list(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_match_player_removed(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_match_all_placed(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_match_new_board(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_match_win(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_match_lose(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_match_end(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+static void parse_match_got_kicked(pugi::xml_document &doc, msg_parsing *msg) {
+	
+}
+
+void parse_message(std::string &xml_message, msg_parsing *msg) {
+	pugi::xml_document doc;
+	doc.load_string(xml_message.c_str());
+	msg->msg_type = get_msg_type(doc);
+
+	switch (msg->msg_type) {		
+		case MSG_PLAYER_GET_OWN_ID:
+			parse_player_get_own_id(doc, msg);
+			break;
+		case MSG_PLAYER_SHIP_PLACEMENT:
+			parse_player_ship_placement(doc, msg);
+			break;
+		case MSG_PLAYER_GET_BOARD:
+			parse_player_get_board(doc, msg);
+			break;
+		case MSG_PLAYER_GET_BOARD_LOST:
+			parse_player_get_board_lost(doc, msg);
+			break;
+		case MSG_PLAYER_ATTACK:
+			parse_player_attack(doc, msg);
+			break;
+		
+		case MSG_HOST_PLAYER_KICK:
+			parse_host_player_kick(doc, msg);
+			break;
+
+		case ACK_MSG_PLAYER_GET_OWN_ID:
+			parse_ack_player_get_own_id(doc, msg);
+			break;
+		case ACK_MSG_MATCH_ATTACK_STATUS:
+			parse_ack_match_attack_status(doc, msg);
+			break;
+		case ACK_MSG_GET_BOARD:
+			parse_ack_get_board(doc, msg);
+			break;
+		case ACK_MSG_GET_BOARD_LOST:
+			parse_ack_get_board_lost(doc, msg);
+			break;
+		case ACK_MSG_MATCH_END:
+			parse_ack_match_end(doc, msg);
+			break;
+		case ACK_INVALID_SHIP_PLACEMENT:
+			parse_ack_invalid_ship_placement(doc, msg);
+			break;
+		case ACK_MSG_MATCH_ATTACK_ERR:
+			parse_ack_match_attack_err(doc, msg);
+			break;
+		case ACK_MSG_MATCH_NOT_HOST:
+			parse_ack_match_not_host(doc, msg);
+			break;
+		case ACK_MSG_MATCH_NOT_DEAD:
+			parse_ack_match_not_dead(doc, msg);
+			break;
+		
+		case MSG_PLAYER_LIST:
+			parse_player_list(doc, msg);
+			break;
+		case MSG_MATCH_PLAYER_REMOVED:
+			parse_match_player_removed(doc, msg);
+			break;
+		case MSG_MATCH_ALL_PLACED:
+			parse_match_all_placed(doc, msg);
+			break;
+		case MSG_MATCH_NEW_BOARD:
+			parse_match_new_board(doc, msg);
+			break;
+		case MSG_MATCH_WIN:
+			parse_match_win(doc, msg);
+			break;
+		case MSG_MATCH_LOSE:
+			parse_match_lose(doc, msg);
+			break;
+		case MSG_MATCH_END:
+			parse_match_end(doc, msg);
+			break;
+		case MSG_MATCH_GOT_KICKED:
+			parse_match_got_kicked(doc, msg);
+			break;
+		
+		default:
+			break;
+	}
 }
