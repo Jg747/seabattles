18a65871e8a819d177b184a5b2b153e5932bcfa8
Your Name <you@example.com>
2026-03-10 21:16:37 +0100

    Removed unnecessary files, all pushed to correct branches

--- C++/include/lang.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef __lang_h__
-#define __lang_h__
-
-#define TITLE "Sea Battles"
-#define GAME_VERSION "v0.0.4"
-
-#endif
--- Java/seabattles/src/main/java/org/seabattles/interfaces/BoardInterface.java
+++
@@ -1,7 +0,0 @@
-package org.seabattles.interfaces;
-
-import org.seabattles.src.Ship;
-
-public interface BoardInterface {
-	public Ship[] getShips();
-}
